773e078a8963d8d5b7b17777763785df911e9953
GustavoCosta2201 <111437501+GustavoCosta2201@users.noreply.github.com>
2024-01-31 10:06:36 -0300

    c#

---
+++ Curso de C#/C# Web crie aplicações usando ASP.NET/C# Web/ScreenSound/Migrations/20240130141359_PopularMusicas.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularMusicas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] {"Veigh", 2023});
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] {"Little Hair", 2022});
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
---
+++ Curso de C#/C# Web crie aplicações usando ASP.NET/Script/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿create table Artistas(
+        Id INT PRIMARY KEY IDENTITY(1,1),
+        Nome NVARCHAR(255) NOT NULL,
+        Bio NVARCHAR(255) NOT NULL,
+        FotoPerfil NVARCHAR(255) NOT NULL
+);
+
+INSERT INTO Artistas (Nome, Bio, FotoPerfil) VALUES ( 'Little Hair', 'Mc Cabelinho', '' );
+
+SELECT * FROM Artistas;
